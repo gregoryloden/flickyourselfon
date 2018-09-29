6dff7dfe2729c03589e69477972335f1995932c8
Gregory Loden <gregory.loden@gmail.com>
2018-09-29 17:32:01 -0400

    add simple update and rendering loops on separate threads

--- Source/flickyourselfon.cpp
+++ Source/flickyourselfon.cpp
@@ -1,6 +1,14 @@
 #include "flickyourselfon.h"
 #include <SDL.h>
 #include <SDL_image.h>
+#include <thread>
+
+SDL_Rect drawRect {0, 0, 11, 19};
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+SDL_Texture* player = nullptr;
+unsigned int updatesPerSecond = 48;
+bool needsNewRenderer = false;
 
 #ifdef __cplusplus
 extern "C"
@@ -10,16 +18,15 @@ int main(int argc, char *argv[]) {
 	if (initResult < 0)
 		return initResult;
 
-	SDL_Window* window = SDL_CreateWindow(
+	window = SDL_CreateWindow(
 		"Flick Yourself On", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 150, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_Texture* player = loadImageTexture(renderer, "images/player.png");
+	player = loadImageTexture(renderer, "images/player.png");
+	std::thread renderLoopThread (renderLoop);
 
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, player, nullptr, nullptr);
-	SDL_RenderPresent(renderer);
-
+	unsigned int startTime = SDL_GetTicks();
+	unsigned int stateNum = 0;
 	SDL_Event gameEvent;
 	while (true) {
 		if (SDL_PollEvent(&gameEvent)) {
@@ -29,9 +36,16 @@ int main(int argc, char *argv[]) {
 					SDL_DestroyRenderer(renderer);
 					SDL_DestroyWindow(window);
 					SDL_Quit();
-					return 0;
+					std::exit(0);
+				case SDL_MOUSEBUTTONDOWN:
+					drawRect.x = (drawRect.x + drawRect.w) % 99;
+					break;
 			}
 		}
+		stateNum++;
+		if (stateNum % (updatesPerSecond / 4) == 0)
+			drawRect.y = (drawRect.y + drawRect.h) % 76;
+		SDL_Delay(1000 * stateNum / updatesPerSecond - (SDL_GetTicks() - startTime));
 	}
 }
 SDL_Texture* loadImageTexture(SDL_Renderer* renderer, const char* path) {
@@ -39,4 +53,11 @@ SDL_Texture* loadImageTexture(SDL_Renderer* renderer, const char* path) {
 	SDL_Texture* image = SDL_CreateTextureFromSurface(renderer, imageSurface);
 	SDL_FreeSurface(imageSurface);
 	return image;
+}
+void renderLoop() {
+	while (true) {
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, player, &drawRect, &drawRect);
+		SDL_RenderPresent(renderer);
+	}
 }
--- Source/flickyourselfon.h
+++ Source/flickyourselfon.h
@@ -2,3 +2,4 @@ struct SDL_Renderer;
 struct SDL_Texture;
 
 SDL_Texture* loadImageTexture(SDL_Renderer* renderer, const char* path);
+void renderLoop();
