0093f55aa6acc556b7f9661e5f88e1d2f8f891e8
Gregory Loden <gregory.loden@gmail.com>
2024-04-12 18:17:00 -0400

    update win tile for level 6

--- Source/GameState/GameState.cpp
+++ Source/GameState/GameState.cpp
@@ -327,8 +327,8 @@ void GameState::render(int ticksTime) {
 		renderTextDisplay(gameTicksTime);
 
 	//TODO: real win condition
-	int winTileX = 134;
-	int winTileY = 116;
+	int winTileX = 162;
+	int winTileY = 124;
 	float px = playerState.get()->getRenderCenterWorldX(gameTicksTime);
 	float py = playerState.get()->getRenderCenterWorldY(gameTicksTime);
 	if (abs(px / MapState::tileSize - winTileX - 0.5f) <= 1.5f
