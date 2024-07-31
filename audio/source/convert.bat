af9826d28f9e89ce8db420e26f3d7ef97e430c7b
Gregory Loden <gregory.loden@gmail.com>
2024-07-31 13:53:27 -0400

    space out flicks even more

--- audio/source/convert.bat
+++ audio/source/convert.bat
@@ -16,4 +16,4 @@ del "slow walking sped up.wav"
 "%ffmpeg%" -i kicking.m4a -filter_complex "[0:a]volume=1,atrim=start=1.98:end=2.13[a0];[0:a]volume=1,atrim=start=3.18:end=3.33[a1];[a0][a1]amix=inputs=2[a2];[a2]asetrate=74167[outa]" -map "[outa]" ..\kick.wav -y
 
 REM original times are 3.66-3.86, 5.52-5.72, 7.31-7.51, 9.09-9.29, the 4th flick was discarded and the others were spaced out to be better distinguishable
-"%ffmpeg%" -i "flick switch.m4a" -filter_complex "[0:a]volume=1,atrim=start=3.650:end=3.850[a1];[0:a]volume=1,atrim=start=5.515:end=5.715[a2];[0:a]volume=1,atrim=start=7.31:end=7.51[a3];[a1][a2][a3]amix=inputs=3,asetrate=55563[outa]" -map "[outa]" "..\switch on.wav" -y
+"%ffmpeg%" -i "flick switch.m4a" -filter_complex "[0:a]volume=1,atrim=start=3.64:end=3.84[a1];[0:a]volume=1,atrim=start=5.51:end=5.71[a2];[0:a]volume=1,atrim=start=7.31:end=7.51[a3];[a1][a2][a3]amix=inputs=3,asetrate=55563[outa]" -map "[outa]" "..\switch on.wav" -y
